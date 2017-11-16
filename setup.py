56a1b1bc2d2c7df76bff9487795caf51f236a8f7
moritzp <moritz.ploss@googlemail.com>
2017-11-16 11:35:30 +0100

    travis ci file edited

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='tribology',
-   version_format='0.1.{commitcount}',
+   version_format='0.2.1',
    setup_requires=['setuptools-git-version'],
    description='methods and classes for tribology research',
    long_description='collection of methods and classes for tribology research '
