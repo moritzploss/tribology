809857493da3482a3c6cdb196511ba206df74d38
Moritz Ploß <moritz.ploss@gmail.com>
2017-11-19 17:57:43 +0100

    Bump version: 0.2.12 → 0.2.13

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='tribology',
-   version='0.2.12',
+   version='0.2.13',
    setup_requires=['setuptools-git-version'],
    description='methods and classes for tribology research',
    long_description='collection of methods and classes for tribology research '
