7a9777679ebb62bf61f66afeaba86e667f4820eb
Moritz Ploß <moritz.ploss@gmail.com>
2017-11-21 23:16:35 +0100

    data_imports added

--- tribology/__init__.py
+++ tribology/__init__.py
@@ -7,13 +7,11 @@ __all__ = ['tribology',
            'constants.py',
            'dowson_hamrock.py',
            'hertz.py',
-           'lubrication.py',
-           'data_import']
+           'lubrication.py']
 
 from tribology.tribology import *
 from tribology.boundary_element import *
 from tribology.constants import *
-from tribology.data_import import *
 from tribology.dowson_hamrock import *
 from tribology.hertz import *
 from tribology.lubrication import *
