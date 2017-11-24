2a6e5eac736d59824834803e3e59527d3fefccba
Moritz Ploß <moritz.ploss@gmail.com>
2017-11-24 13:21:04 +0100

    deploy patch

--- deploy.sh
+++ deploy.sh
@@ -1,26 +1,29 @@
+#!/usr/bin/env bash
 
-if [ "$2" == "major" ] || [ "$2" == "minor" ] || [ "$2" == "patch" ] || [ "$2" == "p" ] ;
+if [ "$1" == "major" ] || [ "$1" == "minor" ] || [ "$1" == "patch" ] || [ "$1" == "p" ] ;
 then
 
 sphinx-build -b html docs/sphinx_files/ docs/ -a
 git add .
 git commit -m "deploy $1"
 
-if [ "$2" ==  "major" ]
+if [ "$1" ==  "major" ]
 then
-    echo $2
-    #bumpversion major
-elif [ "$2" ==  "minor" ]
+echo $1
+#bumpversion major
+elif [ "$1" ==  "minor" ]
 then
-    echo $2
-    #bumpversion minor
-elif [ "$2" == "patch" ] || [ "$2" == "p" ];
+echo $1
+#bumpversion minor
+elif [ "$1" == "patch" ] || [ "$1" == "p" ];
 then
-    echo $2
-    #bumpversion patch
+echo $1
+#bumpversion patch
 fi
+
 git push origin master && git push origin master --tags
-echo "deployed successfully"
+echo "deployed successfully as $1"
+
 else
-echo "parameter '$2' not defined"
+echo "parameter '$1' not defined"
 fi
