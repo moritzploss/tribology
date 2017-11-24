3f7c30d3ca0e199fa2ee867915db28689752f38a
moritzp <moritz.ploss@googlemail.com>
2017-11-24 16:27:43 +0100

    data_import module added, docs still missing

--- tribology/data_import.py
+++ tribology/data_import.py
@@ -98,66 +98,76 @@ def __write_to_out_dict(num_data, column_headers):
     return output_dict
 
 
-def __process_file(in_file, decimal_mark, padding=0):
+def __process_header(col_headers, split_line, prev_line):
+    # replace non-alphanumeric characters and trailing underscores
+    # in column headers
+    col_headers[:] = (re.sub("\W+", '_', item.lower()).strip('_')
+                      for item in prev_line)
+    # convert data type for easy matlab export
+    col_headers = np.asarray(col_headers, dtype='object')
+    # write first line to data table. note that "to_float" is not
+    # necessary, but it's much faster
+    num_dat = np.asarray(
+        [__to_float(item.rstrip('\n'))
+         for item in split_line]).reshape((1, len(split_line)))
+
+    return col_headers, num_dat
+
+
+def __process_data(split_line, num_dat, max_len, num_data_tables):
+    # if data table becomes large, make new data table and add old
+    # table to table list (for speed)
+    if num_dat.shape[0] == max_len:
+        num_data_tables.append(num_dat)
+        num_dat = np.asarray(
+            [__to_float(item.rstrip('\n')) for item in
+             split_line]).reshape(
+            (1, len(split_line)))
+    # else simply append to data table
+    else:
+        num_dat = np.append(num_dat, np.asarray(
+            [__to_float(item.rstrip('\n')) for item in split_line])
+                            .reshape((1, len(split_line))), axis=0)
+    return num_dat
+
+
+def __process_file(in_file, decimal_mark, deli, padding=0):
     """
     extract data from tab-separated text file and return dictionary containing
     all data. the 'padding' parameter allows to ignore leading columns
     (from the left), i.e., if padding = 8, the first 8 columns are ignored.
     """
-    max_num_data_length = 1000
-    num_data = []
-    column_headers = []
+    max_len = 1000
+    num_dat = []
+    col_headers = []
     num_data_tables = []
+    prev_line = ''
     with open(in_file) as dat_file:
         for line in dat_file:
-            if decimal_mark != '.':
-                split_line = line.replace(decimal_mark, '.').split('\t')
-            else:
-                split_line = line.split('\t')
+            split_line = line.replace(decimal_mark, '.').split(deli)
             # get rid of trailing newline characters
-            if split_line and split_line[-1] == '\n':
+            if split_line[-1] == '\n':
                 split_line[-1] = ''
             # check if first character is not (digit or minus symbol (hyphen))
             # to identify non-data lines
             if not (line[0].isdigit() or line[0] == '-') or len(split_line) < 1:
                 if split_line != ['']:
-                    previous_line = split_line
+                    prev_line = split_line
                 continue
             # if line contains data, split line into data fields, fill empty
             # fields with 'nan'
             split_line[:] = (item or 'nan' for item in split_line)
             # if this is the first data-containing line...
-            if not len(column_headers):
-                # replace non-alphanumeric characters and trailing underscores
-                # in column headers
-                column_headers[:] = (re.sub("\W+", '_', item.lower()).strip('_')
-                                     for item in previous_line)
-                # convert data type for easy matlab export
-                column_headers = np.asarray(column_headers, dtype='object')
-                # write first line to data table. note that "to_float" is not
-                # necessary, but it's much faster
-                num_data = np.asarray([__to_float(item.rstrip('\n')) for item in
-                                       split_line]).reshape(
-                    (1, len(split_line)))
-            # ...else append current line to data table
+            if not len(col_headers):
+                col_headers, num_dat = __process_header(col_headers, split_line,
+                                                        prev_line)
             else:
-                # if data table becomes large, make new data table and add old
-                # table to table list (for speed)
-                if num_data.shape[0] == max_num_data_length:
-                    num_data_tables.append(num_data)
-                    num_data = np.asarray(
-                        [__to_float(item.rstrip('\n')) for item in
-                         split_line]).reshape(
-                        (1, len(split_line)))
-                # else simply append to data table
-                else:
-                    num_data = np.append(num_data, np.asarray(
-                        [__to_float(item.rstrip('\n')) for item in split_line])
-                                         .reshape((1, len(split_line))), axis=0)
+                num_dat = __process_data(split_line, num_dat, max_len,
+                                         num_data_tables)
 
-    num_data_tables.append(num_data)
-    num_data = __assemble_data_table(num_data_tables, max_num_data_length)
-    output_dict = __write_to_out_dict(num_data, column_headers)
+    num_data_tables.append(num_dat)
+    num_dat = __assemble_data_table(num_data_tables, max_len)
+    output_dict = __write_to_out_dict(num_dat, col_headers)
 
     return output_dict
 
@@ -177,7 +187,7 @@ def __parse_args():
                         help='specify output database format. default is "npz"'
                              ' for numpy database. use "mat" for matlab '
                              ' database format.')
-    parser.add_argument('-r', '--recursive', action="store_true", default=False,
+    parser.add_argument('-r', '--recursive', action="store_true", default=True,
                         help='recursively walk through all sub-directories of'
                              ' current working directory')
     args = parser.parse_args()
@@ -196,58 +206,100 @@ def __get_file_handles(directory, ext, recursive=False):
     return in_files
 
 
-def __save_out_file(f_no_ext, output_dict, out_ext):
+def __save_out_file(f_no_ext, output_dict, out_ext, out_dir):
     out_file = ''
     if out_ext == 'mat':
-        out_file = '{}/{}.mat'.format(os.getcwd(), f_no_ext)
+        out_file = '{}/{}.mat'.format(out_dir, f_no_ext)
         scipy.io.savemat(out_file, output_dict)
     elif out_ext == 'npz':
-        out_file = '{}/{}'.format(os.getcwd(), f_no_ext)
+        out_file = '{}/{}.npz'.format(out_dir, f_no_ext)
         np.savez(out_file, **output_dict)
     return out_file
 
 
-def import_txt(in_file, force=False, deli='\t', dec_mark='.',
-               out_ext='npz', recursive=False, status=False):
+def import_txt(in_file, force=False, deli='\t', dec_mark='.', out_ext='npz',
+               out_dir=os.getcwd()):
     """
     orchestrate data import
     """
 
     file_no_ext = os.path.splitext(in_file)[0]
-    out_file_exists = (".".join([file_no_ext, out_ext])
-                       in os.path.dirname(in_file))
+    out_file = os.sep.join([out_dir, ".".join([file_no_ext, out_ext])])
+    out_file_exists = (os.path.isfile(out_file))
 
-    import_status = False
-    out_file = ''
+    import_status = None
+    out_file = None
 
-    if not out_file_exists or force is True:
+    if (not out_file_exists) or (force is True):
         try:
-            output_dict = __process_file(in_file, dec_mark)
-            out_file = __save_out_file(file_no_ext, output_dict, out_ext)
+            output_dict = __process_file(in_file, dec_mark, deli)
+            out_file = __save_out_file(file_no_ext, output_dict, out_ext,
+                                       out_dir)
             import_status = True
         except ValueError:
-            pass
+            import_status = False
     return out_file, import_status
 
 
-if __name__ == "__main__":
-    """
-    args = __parse_args()
-    in_files = __get_file_handles(os.getcwd(), args.ext, args.recursive)
-    for in_file in in_files:
-        import_txt(in_files, force=args.force, deli=args.delimiter,
-               dec_mark=args.mark, out_ext=args.outformat)
-        __print_status("file '{}.{}' already exists. did not overwrite {} "
-                       "database".format(f_no_ext,
-                                         args.outformat,
-                                         args.outformat),
-                       __bcolors.ENDC)
-        print_string = "file '{}' imported. {} database".format(in_file,
-                                                                out_ext)
-        if out_file_exists:
-            __print_status("{} overwritten".format(print_string),
-                           __bcolors.OKGREEN)
+def __print_import_stats(print_stat, in_file, status):
+    if print_stat:
+        out_str = ': '.join([str(status), str(in_file)])
+        if status is False:
+            out_col = __bcolors.FAIL
+        elif status is True:
+            out_col = __bcolors.OKGREEN
         else:
-            __print_status("{} created".format(print_string),
-                           __bcolors.OKGREEN)
+            out_col = __bcolors.WARNING
+        __print_status(out_str, out_col)
+
+
+def import_dir(in_dir, in_ext, recursive=False, force=False, deli='\t',
+               dec_mark='.',  out_ext='npz', out_dir=os.getcwd(),
+               print_stat=False):
     """
+
+
+    Parameters
+    ----------
+    in_dir: str
+        Path to directory (top-level directory in case of `recursive=True`)
+    in_ext: str
+        File extension of files to import (without dot).
+    recursive: bool, optional
+        Default `False`. If `True`, all files in `in_dir` and all its child
+        directories are imported. Output files are saved to the same directory
+        as source files irrespective of `out_dir` value.
+    force
+    deli
+    dec_mark
+    out_ext
+    out_dir
+    print_stat
+
+    Returns
+    -------
+
+    """
+    in_files = __get_file_handles(in_dir, in_ext, recursive)
+    out_files = []
+    import_status = []
+
+    if print_stat:
+        print('importing {} files:'.format(len(in_files)))
+
+    for in_file in in_files:
+
+        out_file, status = import_txt(in_file, force=force, deli=deli,
+                                      dec_mark=dec_mark, out_ext=out_ext,
+                                      out_dir=out_dir)
+        out_files.append(out_file)
+        import_status.append(status)
+        __print_import_stats(print_stat, in_file, status)
+
+    return out_files, import_status
+
+
+if __name__ == "__main__":
+    args = __parse_args()
+    import_dir(os.getcwd(), args.extension, args.recursive, args.force,
+               args.delimiter, args.mark, args.outformat, os.getcwd(), True)
