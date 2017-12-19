5882cf63500d65f8b6ae8577ea79520aa9bf07b7
moritzp <moritz.ploss@googlemail.com>
2017-12-19 11:43:56 +0100

    deploy patch

--- docs/sphinx_files/conf.py
+++ docs/sphinx_files/conf.py
@@ -19,7 +19,12 @@
 #
 import os
 import sys
+import mock
 sys.path.insert(0, os.path.abspath('../../tribology'))
+
+MOCK_MODULES = ['tribology.boundary_element']
+for mod_name in MOCK_MODULES:
+  sys.modules[mod_name] = mock.MagicMock()
 
 
 # -- General configuration ------------------------------------------------
