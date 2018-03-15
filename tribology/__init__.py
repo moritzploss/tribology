89e9eb5f6dfa6057ffb60cce97c4fd8aba200df7
moritzp <moritz.ploss@googlemail.com>
2018-03-15 16:54:57 +0100

    changed init configuration

--- tribology/__init__.py
+++ tribology/__init__.py
@@ -25,4 +25,4 @@ from tribology.roller_bearings import *
 
 import sys
 sys.path.insert(0, "p3can/")
-from tribology.p3can import p3can, generate_input_file
+from tribology.p3can.p3can import p3can, generate_input_file
