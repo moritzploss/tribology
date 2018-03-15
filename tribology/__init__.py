ef0ae03ada6665bced90eb144787259b27c23a99
moritzp <moritz.ploss@googlemail.com>
2018-03-15 16:57:31 +0100

    changed init configuration

--- tribology/__init__.py
+++ tribology/__init__.py
@@ -22,7 +22,4 @@ from tribology.dowson_hamrock import *
 from tribology.hertz import *
 from tribology.lubrication import *
 from tribology.roller_bearings import *
-
-import sys
-sys.path.insert(0, "p3can/")
-from tribology.p3can.p3can import p3can, generate_input_file
+from tribology.p3can import *
