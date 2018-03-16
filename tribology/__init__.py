9a679755c8f0e27531ceb753c4eed1758068b7b7
moritzp <moritz.ploss@googlemail.com>
2018-03-16 09:49:35 +0100

    deploy patch

--- tribology/__init__.py
+++ tribology/__init__.py
@@ -21,3 +21,14 @@ from tribology.dowson_hamrock import *
 from tribology.hertz import *
 from tribology.lubrication import *
 from tribology.roller_bearings import *
+
+import sys
+packpath = os.sep.join(os.path.realpath(__file__).split(os.sep)[:-1])
+sys.path.insert(0, packpath)
+
+subs_dirs = os.walk(packpath)
+for sub_dir in subs_dirs:
+    if not sub_dir[0].endswith('__pycache__'):
+        sys.path.insert(0, sub_dir[0])
+
+pass
