65d78f65a60926eb31f7eff17a981e35b1ac268e
moritzp <moritzp@kth.se>
2018-11-13 16:05:28 +0100

    Bump version: 0.4.9 → 0.5.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='tribology',
-   version='0.4.9',
+   version='0.5.0',
    setup_requires=['setuptools-git-version'],
    description='functions and classes for tribology research',
    long_description='A collection of functions and classes for tribology research '
