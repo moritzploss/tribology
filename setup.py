09bf75d670fb3d86575ca4bdced00d5dce2d4af7
Moritz Ploss <moritz.ploss@gmail.com>
2019-08-19 11:08:48 +0200

    Bump version: 0.5.15 → 0.5.16

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='tribology',
-   version='0.5.15',
+   version='0.5.16',
    setup_requires=['setuptools-git-version'],
    description='functions and constants for tribology research',
    long_description='A collection of functions and constants for tribology '
